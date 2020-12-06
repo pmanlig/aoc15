d246280b0acdca5c78f1d1086898216803c300cb
pmanlig <patrik@manlig.org>
2020-12-06 01:37:56 +0100

    Day 8

--- src/Solvers/S8.js
+++ src/Solvers/S8.js
@@ -3,9 +3,22 @@ import Solver from './Solver';
 
 export class S8a extends Solver {
 	solve(input) {
-		this.setState({ solution: "No solution yet" });
+		input = input.replace(/\n/g, "");
+		let mem = input.replace(/\\x..|\\\\|\\"/g, "?");
+		mem = mem.replace(/"/g, "");
+		this.setState({ solution: `Characters: ${input.length - mem.length}` });
 	}
 }
 
 export class S8b extends Solver {
+	solve(input) {
+		let i = 0;
+		let enc = input.split('\n').map(l => {
+			l = l.replace(/\\/g, "\\\\");
+			l = l.replace(/"/g, "\\\"");
+			return `"${l}"`;
+		}).reduce((a, b) => a + b, "");
+		input = input.replace(/\n/g, "");
+		this.setState({ solution: `Characters: ${enc.length - input.length}` });
+	}
 }
