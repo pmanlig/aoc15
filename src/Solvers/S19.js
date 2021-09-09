d83cc31b6b02c31f0fdfa785853fc2ef275767fc
pmanlig <patrik@manlig.org>
2021-09-09 13:50:32 +0200

    Day 20

--- src/Solvers/S19.js
+++ src/Solvers/S19.js
@@ -2,8 +2,70 @@
 import Solver from './Solver';
 
 export class S19a extends Solver {
+	operate(mol, rules, result) {
+		result = result || new Set();
+		for (let i = 0; i < mol.length; i++) {
+			for (let j = 0; j < rules.length; j++) {
+				let l = rules[j][0].length;
+				if (i + l - 1 < mol.length) {
+					if (mol.slice(i, i + l) === rules[j][0]) {
+						result.add(mol.slice(0, i) + rules[j][1] + mol.slice(i + l));
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	reverseOperate(search, rules, seen) {
+		let b = 0;
+		for (let i = 1; i < search.length; i++) {
+			if (search[i].data.length < search[b].data.length ||
+				(search[i].data.length === search[b].data.length && search[i].steps < search[b].steps)) {
+				b = i;
+			}
+		}
+		let data = search[b].data, steps = search[b].steps;
+		search.splice(b, 1);
+		if (data !== "e") {
+			if (!seen.has(data)) {
+				seen.add(data);
+				for (let i = 0; i < data.length; i++) {
+					for (let j = 0; j < rules.length; j++) {
+						let l = rules[j][1].length;
+						if (i + l - 1 < data.length) {
+							if (data.slice(i, i + l) === rules[j][1]) {
+								let n = data.slice(0, i) + rules[j][0] + data.slice(i + l);
+								search = search.filter(r => r.data !== n);
+								search.push({ steps: steps + 1, data: n });
+							}
+						}
+					}
+				}
+			}
+			setTimeout(() => { this.reverseOperate(search, rules, seen) }, 1);
+		}
+		this.setState({ molecule: data, steps: steps });
+	}
+
 	solve(input) {
-		this.setState({ solution: "No solution yet" });
+		// input = "H => HO\nH => OH\nO => HH\n\nHOH";
+		// input = "H => HO\nH => OH\nO => HH\n\nHOHOHO";
+		input = input.split('\n');
+		let mol = input[input.length - 1];
+		let rules = input.slice(0, -2).map(s => /(\w+) => (\w+)/.exec(s).slice(1, 3));
+		let result = this.operate(mol, rules);
+		this.setState({ calibration: result.size, steps: 0, molecule: mol });
+		setTimeout(() => { this.reverseOperate([{ steps: 0, data: mol }], rules, new Set()); }, 1);
+	}
+
+	customRender() {
+		return <div>
+			<p>Calibration: {this.state.calibration}</p>
+			<p>Best sequence: {this.state.steps}</p>
+			<p>Current sequence: {this.state.molecule}</p>
+			<p>Current length: {this.state.molecule.length}</p>
+		</div>;
 	}
 }
 
--- src/Solvers/S20.js
+++ src/Solvers/S20.js
@@ -2,22 +2,32 @@
 import Solver from './Solver';
 
 export class S20a extends Solver {
-	presents(i) {
-		let elf = 0, p = i * 10, half = Math.floor(i / 2);
-		while (elf++ < half) if (i % elf === 0) p += 10 * elf;
-		return p;
-	}
-
 	solve(input) {
 		input = parseInt(input, 10);
-		let house = input;
-		while (this.presents(house) < input) {
-			house++;
+		let max = input / 10;
+		let houses = [];
+		for (let i = 1; i <= max; i++) { houses[i] = 10; }
+		for (let i = 2; i <= max; i++) {
+			for (let j = i; j <= max; j += i) {
+				houses[j] += 10 * i;
+			}
 		}
-		console.log("Done");
-		this.setState({ solution: `House #: ${house}` });
+		let h1 = 0, h2 = 0;
+		while (houses[++h1] < input);
+		for (let i = 1; i <= max; i++) { houses[i] = 0; }
+		for (let i = 1; i <= max; i++) {
+			for (let j = 1; j <= 50; j++) {
+				if (i * j > max) break;
+				houses[i * j] += 11 * i;
+			}
+		}
+		while (houses[++h2] < input);
+		console.log("Houses", houses);
+		this.setState({ solution: `House: ${h1}\nHouse (lazy): ${h2}` });
+		// setTimeout(() => { this.increment(1, input) }, 1);
 	}
 }
+
 
 export class S20b extends Solver {
 }
